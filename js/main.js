0391b2ffe9e2545d6e031773b175f91fb58a8dac
shukurullohj <shukurulloh.jamolov@mail.ru>
2024-01-11 13:58:53 +0500

    get information from JS

--- js/main.js
+++ js/main.js
@@ -21,3 +21,47 @@ const data = [
   }
 ];
 
+document.addEventListener("DOMContentLoaded", (event) => {
+  const elStatsList = document.querySelector('.stats-list');
+  const elStatsContentFragment = document.createDocumentFragment();
+
+  data.forEach( function (item) {
+    const elStatsItem = document.createElement('li');
+    elStatsItem.classList.add('stats-list__item', `stats-list__item--${item.category.toLowerCase()}`);
+
+    const elStatsItemImg = document.createElement('img');
+    elStatsItemImg.classList.add('stats-list__item-icon');
+    elStatsItemImg.src = item.icon;
+    elStatsItemImg.width = "20";
+    elStatsItemImg.height = "20";
+
+
+
+    const elStatsItemTitle = document.createElement('span');
+    elStatsItemTitle.classList.add('stats-list__item-title');
+    elStatsItemTitle.textContent = item.category;
+
+    const elStatsItemScore = document.createElement('span');
+    elStatsItemScore.classList.add('stats-list__item-result');
+    elStatsItemScore.textContent = item.score;
+
+    const elStatsItemPercentLabel = document.createElement('span');
+    elStatsItemPercentLabel.classList.add('stats-list__item-percent-label');
+    elStatsItemPercentLabel.textContent = "%";
+
+
+    const elStatsItemMax = document.createElement('span');
+    elStatsItemMax.classList.add('stats-list__item-max');
+    elStatsItemMax.textContent = "/ 100";
+    
+    elStatsItem.appendChild(elStatsItemImg);
+    elStatsItem.appendChild(elStatsItemTitle);
+    elStatsItem.appendChild(elStatsItemScore);
+    elStatsItem.appendChild(elStatsItemPercentLabel);
+    elStatsItem.appendChild(elStatsItemMax);
+
+    elStatsContentFragment.appendChild(elStatsItem); 
+  });
+
+   elStatsList.appendChild(elStatsContentFragment);
+})
